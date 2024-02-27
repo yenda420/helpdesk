2353c819ac40f77b37141076d604bdfb2341914a
finnlinn <106872018+finnlinn@users.noreply.github.com>
2024-02-27 15:34:05 +0100

    pridani stylu + smazani zbytecnych souboru

--- helpdesk/prevence/start.php
+++ helpdesk/prevence/start.php
@@ -46,6 +46,7 @@ if(isset($_POST['request'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles_start.css">
     <title>Helpdesk</title>
 </head>
 <body>
