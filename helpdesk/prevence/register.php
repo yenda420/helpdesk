3ba389ea019b337b386f774e39bcc3b5e03fa33e
yenda420 <161469951+yenda420@users.noreply.github.com>
2024-02-27 15:41:58 +0000

    saf

--- helpdesk/prevence/register.php
+++ helpdesk/prevence/register.php
@@ -5,10 +5,10 @@ include 'config.php';
 if(isset($_POST['submit'])){
 
    $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $surname = mysqli_real_escape_string($conn, $_POST['surname']);
    $email = mysqli_real_escape_string($conn, $_POST['email']);
    $pass = mysqli_real_escape_string($conn, hash('sha256', $_POST['password']));
    $cpass = mysqli_real_escape_string($conn, hash('sha256', $_POST['cpassword']));
-   $user_type = $_POST['user_type'];
 
    $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email' AND password = '$pass'") or die('query failed');
 
@@ -18,7 +18,7 @@ if(isset($_POST['submit'])){
       if($pass != $cpass){
          $message[] = 'Passwords don\'t match!';
       }else{
-         mysqli_query($conn, "INSERT INTO `users` SET userName='$name', email='$email', password='$cpass', user_type='$user_type'") or die('query failed');
+         mysqli_query($conn, "INSERT INTO `users` SET userName='$name', userSurname='$surname', email='$email', userPasswd='$pass', userType='frontend'") or die('query failed');
          $message[] = 'Request for an account was successful!';
          header('location:index.php');
       }
@@ -34,7 +34,7 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Request for an account</title>
+   <title>Request</title>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
    <link rel="stylesheet" href="css/style.css">
 
@@ -59,18 +59,14 @@ if(isset($message)){
 <div class="form-container">
 
    <form action="" method="post">
-      <h3>Request</h3>
+      <h3>Request an account</h3>
       <input type="text" name="name" placeholder="Name" required class="box">
-      <input type="text" name="name" placeholder="Surname" required class="box">
+      <input type="text" name="surname" placeholder="Surname" required class="box">
       <input type="email" name="email" placeholder="Email" required class="box">
       <input type="password" name="password" placeholder="Create password" required class="box">
       <input type="password" name="cpassword" placeholder="Confirm password" required class="box">
-      <select name="user_type" class="box">
-         <option value="user">uživatel</option>
-         <option value="admin">správce</option>
-      </select>
-      <input type="submit" name="submit" value="register" class="btn">
-      <p>Už máš účet? <a href="index.php">přihlášení</a></p>
+      <input type="submit" name="submit" value="Send request" class="btn">
+      <p>Have an account? <a href="index.php">Login</a></p>
    </form>
 
 </div>
