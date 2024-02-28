73ded7a65e1d02a93cfb031c66997899286bd228
finnlinn <106872018+finnlinn@users.noreply.github.com>
2024-02-28 11:17:42 +0100

    adsds

--- helpdesk/prevence/header.php
+++ helpdesk/prevence/header.php
@@ -24,18 +24,6 @@ if (isset($message)) {
             <a href=".php">Send a ticket</a>
          </nav>
 
-         <div class="icons">
-            <div id="menu-btn" class="fas fa-bars"></div>
-            <div id="user-btn" class="fas fa-user"></div>
-            <?php
-            $select_cart_number = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
-            $cart_rows_number = mysqli_num_rows($select_cart_number);
-            ?>
-            <a href="cart.php"> <i class="fas fa-shopping-cart"></i> <span>(
-                  <?php echo $cart_rows_number; ?>)
-               </span> </a>
-         </div>
-
          <div class="user-box">
             <p>First Name: <span>
                   <?php echo $_SESSION['user_name']; ?>
---
+++ helpdesk/prevence/home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>AdminSpace</title>
+
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<section class="dashboard">
+   <section class="users">
+
+   <h1 class="title">Requests</h1>
+
+   <div class="box-container">
+      ahoj
+   </div>
+</section>
+</section>
+<script src="js/admin_script.js"></script>
+<?php include 'footer.php'; ?>
+</body>
+</html>
