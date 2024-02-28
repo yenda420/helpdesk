85276cc27d71fc2105aafc729b0110ca7a2c2afd
yenda420 <161469951+yenda420@users.noreply.github.com>
2024-02-28 13:33:38 +0000

    asfaf

--- helpdesk/prevence/admin_tickets.php
+++ helpdesk/prevence/admin_tickets.php
@@ -5,7 +5,7 @@
     require("functions.php");
 
     $users = returnAllUsers($conn);
-
+    
     if (isset($_POST["users"])) {
         $tickets = empty($_POST["users"]) ? returtnAllTickets($conn) : returnTicketsForSelectedUser($conn, $_POST);
     } else {
@@ -15,13 +15,58 @@
     require("admin_header.php");
 ?>
 
-<form method="post">
-    <select name="users" required>
-        <option value="">--- Choose a user ---</option>
-        <?php foreach ($tickets as $ticket) { ?>
-            <option value="<?= $tickets["ticketId"] ?>">ahoj</option>
-        <?php } ?>
-    </select>
+<section class="dashboard">
+    <section class="tickets">
+        <h1 class="title">Tickets</h1>
+    </section>
 
-    <input type="submit" value="Show tickets">
-</form>
+    <form method="post">
+        <select name="users" required>
+            <option selected value="">--- Choose a user ---</option>
+            <?php foreach ($users as $user) { ?>
+                <option value="<?= $user["userId"] ?>"><?= $user["userName"] ?> <?= $user["userSurname"] ?> <?= $user["email"] ?></option>
+            <?php } ?>
+        </select>
+
+        <input type="submit" value="Show tickets">
+    </form>
+
+    <?php if (empty($_POST["users"])) { ?>
+
+        <div class="box-container">
+            <?php 
+                foreach ($tickets as $ticket) { 
+                    $user = returnUserForSelectedTicket($conn, $ticket["ticketId"]);
+            ?>
+                    <p>ID: <span> <?= $ticket["ticketId"] ?></span></p>
+                    <p>Title: <span> <?= $ticket["title"] ?></span></p>
+                    <p>Users name: <span> <?= $user["userName"] ?></span></p>
+                    <p>Users surname: <span> <?= $user["userSurname"] ?></span></p>
+                    <p>Users email: <span> <?= $user["email"] ?></span></p>
+                    <p>Ticket type: <span> <?= $ticket["ticketType"] ?></span></p>
+                    <p>Description: <span> <?= $ticket["ticketDesc"] ?></span></p>
+            <?php } ?>
+        </div>
+
+        <?php } else {
+            if (isset($_POST["users"]) && $_POST["users"] != 0) { 
+                $user = returnUser($conn, $_POST["users"]);
+
+                if (numberOfTickets($conn, $_POST) != 0) { 
+        ?>
+                    <h1 class="title">Tickets for <?= $user["userName"] ?> <?= $user["userSurname"] ?></h1>
+                    
+                    <?php foreach ($tickets as $ticket) { ?>
+                    <p>ID: <span> <?= $ticket["ticketId"] ?></span></p>
+                    <p>Title: <span> <?= $ticket["title"] ?></span></p>
+                    <p>Ticket type: <span> <?= $ticket["ticketType"] ?></span></p>
+                    <p>Description: <span> <?= $ticket["ticketDesc"] ?></span></p>
+
+                    <?php } 
+                } else { ?>
+                    <h1 class="title"><?= $user["userName"] ?> <?= $user["userSurname"] ?> currently has no tickets</h1>
+                <?php }
+            }
+    } ?>
+</section>
+
--- helpdesk/prevence/functions.php
+++ helpdesk/prevence/functions.php
@@ -22,10 +22,47 @@
             SELECT *
             FROM users u, tickets t
             WHERE u.userId = t.userId
-                AND u.userId = '{$data["userId"]}';
+                AND u.userId = '{$data["users"]}';
         ";
 
         $sqlResult = mysqli_query($conn, $sql);
         $tickets = mysqli_fetch_all($sqlResult, MYSQLI_ASSOC);
         return $tickets;
+    }
+
+    function returnUserForSelectedTicket($conn, $ticketId) {
+        $sql = "
+            SELECT u.userName, u.userSurname, u.email
+            FROM users u, tickets t
+            WHERE u.userId = t.userId
+                AND t.ticketId = $ticketId;
+        ";
+    
+        $sqlResult = mysqli_query($conn, $sql);
+        $user = mysqli_fetch_all($sqlResult, MYSQLI_ASSOC);
+    
+        return $user[0];
+    }
+    
+    function returnUser($conn, $userId) {
+        $sql = "SELECT * FROM users WHERE userId = $userId";
+
+        $sqlResult = mysqli_query($conn, $sql);
+        $user = mysqli_fetch_all($sqlResult, MYSQLI_ASSOC);
+
+        return $user[0];
+    }
+
+    function numberOfTickets($conn, $data) {
+        $sql = "
+            SELECT *
+            FROM users u, tickets t
+            WHERE u.userId = t.userId
+                AND u.userId = '{$data["users"]}';
+        ";
+
+        $sqlResult = mysqli_query($conn, $sql);
+        $numberOfRecords = mysqli_num_rows($sqlResult);
+
+        return $numberOfRecords;
     }
