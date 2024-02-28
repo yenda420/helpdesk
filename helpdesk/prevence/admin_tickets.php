a9908a8e0e555dba7e737972f6f88e38fbddc5ee
finnlinn <106872018+finnlinn@users.noreply.github.com>
2024-02-28 16:12:10 +0100

    saasa

--- helpdesk/prevence/admin_tickets.php
+++ helpdesk/prevence/admin_tickets.php
@@ -1,84 +1,132 @@
 <?php
-    session_start();
+session_start();
 
-    require("config.php");
-    require("functions.php");
+require("config.php");
+require("functions.php");
 
-    $users = returnAllUsers($conn);
-    
-    if (isset($_POST["users"])) {
-        $tickets = empty($_POST["users"]) ? returtnAllTickets($conn) : returnTicketsForSelectedUser($conn, $_POST);
-    } else {
-        $tickets = returtnAllTickets($conn);
-    }
+$users = returnAllUsers($conn);
 
-    require("admin_header.php");
+if (isset($_POST["users"])) {
+    $tickets = empty($_POST["users"]) ? returtnAllTickets($conn) : returnTicketsForSelectedUser($conn, $_POST);
+} else {
+    $tickets = returtnAllTickets($conn);
+}
+
+require("admin_header.php");
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>AdminSpace</title>
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-   <link rel="stylesheet" href="css/admin_style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AdminSpace</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="css/admin_style.css">
 </head>
+
 <body>
-<section class="dashboard">
-    <section class="tickets">
-        <h1 class="title">Tickets</h1>
-    </section>
 
-    <form method="post">
-        <select name="users" required>
-            <option selected value="">--- Choose a user ---</option>
-            <?php foreach ($users as $user) { ?>
-                <option value="<?= $user["userId"] ?>"><?= $user["userName"] ?> <?= $user["userSurname"] ?> <?= $user["email"] ?></option>
-            <?php } ?>
-        </select>
+    <section class="dashboard">
+        <section class="tickets">
+            <h1 class="title">Tickets</h1>
+        </section>
 
-        <input type="submit" value="Show tickets">
-    </form>
+        <form method="post">
+            <div class="flex">
+            <div class="inputBox">
+                <select name="users" required>
+                    <option selected value="">--- Choose a user ---</option>
+                    <?php foreach ($users as $user) { ?>
+                        <option value="<?= $user["userId"] ?>">
+                            <?= $user["userName"] ?>
+                            <?= $user["userSurname"] ?>
+                            <?= $user["email"] ?>
+                        </option>
+                    <?php } ?>
+                </select>
+            </div>
+            <input type="submit" value="Show tickets" class="btn" name="show_tickets">
+            </div>
 
-    <?php if (empty($_POST["users"])) { ?>
+        </form>
 
-        <div class="box-container">
-            <?php 
-                foreach ($tickets as $ticket) { 
+        <?php if (empty($_POST["users"])) { ?>
+
+            <div class="box-container">
+                <?php
+                foreach ($tickets as $ticket) {
                     $user = returnUserForSelectedTicket($conn, $ticket["ticketId"]);
-            ?>
-                    <p>ID: <span> <?= $ticket["ticketId"] ?></span></p>
-                    <p>Title: <span> <?= $ticket["title"] ?></span></p>
-                    <p>Users name: <span> <?= $user["userName"] ?></span></p>
-                    <p>Users surname: <span> <?= $user["userSurname"] ?></span></p>
-                    <p>Users email: <span> <?= $user["email"] ?></span></p>
-                    <p>Ticket type: <span> <?= $ticket["ticketType"] ?></span></p>
-                    <p>Description: <span> <?= $ticket["ticketDesc"] ?></span></p>
-            <?php } ?>
-        </div>
+                    ?>
+                    <div class="box">
+                        <p>ID: <span>
+                                <?= $ticket["ticketId"] ?>
+                            </span></p>
+                        <p>Title: <span>
+                                <?= $ticket["title"] ?>
+                            </span></p>
+                        <p>Users name: <span>
+                                <?= $user["userName"] ?>
+                            </span></p>
+                        <p>Users surname: <span>
+                                <?= $user["userSurname"] ?>
+                            </span></p>
+                        <p>Users email: <span>
+                                <?= $user["email"] ?>
+                            </span></p>
+                        <p>Ticket type: <span>
+                                <?= $ticket["ticketType"] ?>
+                            </span></p>
+                        <p>Description: <span>
+                                <?= $ticket["ticketDesc"] ?>
+                            </span></p>
+                    </div>
+                <?php } ?>
+            </div>
 
         <?php } else {
-            if (isset($_POST["users"]) && $_POST["users"] != 0) { 
+            if (isset($_POST["users"]) && $_POST["users"] != 0) {
                 $user = returnUser($conn, $_POST["users"]);
 
-                if (numberOfTickets($conn, $_POST) != 0) { 
-        ?>
-                    <h1 class="title">Tickets for <?= $user["userName"] ?> <?= $user["userSurname"] ?></h1>
-                    
+                if (numberOfTickets($conn, $_POST) != 0) {
+                    ?>
+                    <h1 class="title">Tickets for
+                        <?= $user["userName"] ?>
+                        <?= $user["userSurname"] ?>
+                    </h1>
+                    <div class="box-container">
                     <?php foreach ($tickets as $ticket) { ?>
-                    <p>ID: <span> <?= $ticket["ticketId"] ?></span></p>
-                    <p>Title: <span> <?= $ticket["title"] ?></span></p>
-                    <p>Ticket type: <span> <?= $ticket["ticketType"] ?></span></p>
-                    <p>Description: <span> <?= $ticket["ticketDesc"] ?></span></p>
+                        
+                    <div class="box">
+                        <p>ID: <span>
+                                <?= $ticket["ticketId"] ?>
+                            </span></p>
+                        <p>Title: <span>
+                                <?= $ticket["title"] ?>
+                            </span></p>
+                        <p>Ticket type: <span>
+                                <?= $ticket["ticketType"] ?>
+                            </span></p>
+                        <p>Description: <span>
+                                <?= $ticket["ticketDesc"] ?>
+                            </span></p>
+                    </div>
 
-                    <?php } 
+                    <?php }
                 } else { ?>
-                    <h1 class="title"><?= $user["userName"] ?> <?= $user["userSurname"] ?> currently has no tickets</h1>
+                    </div>
+                    <h1 class="title">
+                        <?= $user["userName"] ?>
+                        <?= $user["userSurname"] ?> currently has no tickets
+                    </h1>
                 <?php }
             }
-    } ?>
-</section>
+        } ?>
+    </section>
+    <script src="js/admin_script.js"></script>
+    <?php include 'footer.php'; ?>
 </body>
+
 </html>
