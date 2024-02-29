9461d896761f77088383e1885ab0ad785bebf275
yenda420 <161469951+yenda420@users.noreply.github.com>
2024-02-29 10:30:06 +0000

    asd

--- helpdesk/prevence/home.php
+++ helpdesk/prevence/home.php
@@ -33,42 +33,33 @@ session_start();
             $user_email = $_SESSION['user_email'];
             $userId = $_SESSION['user_id'];
             if (numberOfTicketsForUserId($conn, $userId) != 0) {
-            $select_tickets = mysqli_query($conn, "SELECT * FROM `tickets`") or die('query failed');
-            while ($fetch_tickets = mysqli_fetch_assoc($select_tickets)) {
 
-                     
-                        if ($userId == $fetch_tickets['userId']) {
-                           ?>
-                           <div class="box">
-                              <p> Ticket ID: <span>
-                                    <?php echo $fetch_tickets['ticketId']; ?>
-                                 </span> </p>
-                              <p> Title: <span>
-                                    <?php echo $fetch_tickets['title']; ?>
-                                 </span> </p>
-                              <p> Type: <span>
-                                    <?php echo $fetch_tickets['ticketType']; ?>
-                                 </span> </p>
-                              <p> Description: <span>
-                                    <?php echo $fetch_tickets['ticketDesc']; ?>
-                                 </span> </p>
-                              <p> Date: <span>
-                                    <?php echo $fetch_tickets['ticketDate']; ?>
-                                 </span> </p>
-                              <p> User ID: <span>
-                                    <?php echo $fetch_tickets['userId']; ?>
-                                 </span> </p>
-                           </div>
-                           <?php
-                        }
+               $select_tickets = mysqli_query($conn, "SELECT * FROM `tickets`") or die('query failed');
+
+               while ($fetch_tickets = mysqli_fetch_assoc($select_tickets)) {
+                     if ($userId == $fetch_tickets['userId']) {
+                        ?>
+                        <div class="box">
+                           <p> Title: <span>
+                                 <?php echo $fetch_tickets['title']; ?>
+                              </span> </p>
+                           <p> Type: <span>
+                                 <?php echo $fetch_tickets['ticketType']; ?>
+                              </span> </p>
+                           <p> Description: <span>
+                                 <?php echo $fetch_tickets['ticketDesc']; ?>
+                              </span> </p>
+                           <p> Date: <span>
+                                 <?php echo $fetch_tickets['ticketDate']; ?>
+                              </span> </p>
+                        </div>
+                        <?php
                      }
-                     
                   }
-                  else { ?>
-                     <p class="empty">No requests</p>
-                  <?php }
-            ;
-            ?>
+                  
+            } else { ?>
+               <p class="empty">No requests</p>
+            <?php } ?>
 
          </div>
       </section>
