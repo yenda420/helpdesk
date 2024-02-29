3f817e989d71fb2dfe1d6308fdbacb96b5dcbdb3
Páťa <116119282+preisens@users.noreply.github.com>
2024-02-29 12:00:33 +0000

    wrong credentials message

--- helpdesk/prevence/index.php
+++ helpdesk/prevence/index.php
@@ -2,33 +2,34 @@
 
 include 'config.php';
 session_start();
+if (isset($_POST['submit'])) {
 
-if(isset($_POST['submit'])){
-
-   $pass = hash('sha256',mysqli_real_escape_string($conn, $_POST['password']));
+   $pass = hash('sha256', mysqli_real_escape_string($conn, $_POST['password']));
    $email = mysqli_real_escape_string($conn, $_POST['email']);
    //echo $name . " " . $last_name . " " . $pass . "<br>";
 
-  $select_users = mysqli_query($conn, "SELECT * FROM `users` where userPasswd = '$pass' and email='$email'") or die('query failed');
-  //$select_users = mysqli_query($conn, "SELECT * FROM `users` ") or die('query failed');
-  if(mysqli_num_rows($select_users) > 0){
-   while($fetch_users = mysqli_fetch_assoc($select_users)){
-       echo $fetch_users['userName'] . " " . $fetch_users['userSurname'] . " " . $fetch_users['userPasswd'] . " " . $fetch_users['userType'] . "<br>";
-       if($fetch_users['userType'] == 'backend'){
-           $_SESSION['admin_name'] = $fetch_users['userName'];
-           $_SESSION['admin_email'] = $fetch_users['email'];
-           $_SESSION['admin_id'] = $fetch_users['userId'];
-          header('location:admin_page.php');
-       }
-       if($fetch_users['userType'] == 'frontend'){
-           $_SESSION['user_name'] = $fetch_users['userName'];
-           $_SESSION['user_email'] = $fetch_users['email'];
-           $_SESSION['user_id'] = $fetch_users['userId'];
-           $_SESSION['user_surname'] = $fetch_users['userSurname'];
-          header('location:home.php');
-       }
-   }   
-}
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` where userPasswd = '$pass' and email='$email'") or die('query failed');
+   //$select_users = mysqli_query($conn, "SELECT * FROM `users` ") or die('query failed');
+   if (mysqli_num_rows($select_users) > 0) {
+      while ($fetch_users = mysqli_fetch_assoc($select_users)) {
+         echo $fetch_users['userName'] . " " . $fetch_users['userSurname'] . " " . $fetch_users['userPasswd'] . " " . $fetch_users['userType'] . "<br>";
+         if ($fetch_users['userType'] == 'backend') {
+            $_SESSION['admin_name'] = $fetch_users['userName'];
+            $_SESSION['admin_email'] = $fetch_users['email'];
+            $_SESSION['admin_id'] = $fetch_users['userId'];
+            header('location:admin_page.php');
+         }
+         if ($fetch_users['userType'] == 'frontend') {
+            $_SESSION['user_name'] = $fetch_users['userName'];
+            $_SESSION['user_email'] = $fetch_users['email'];
+            $_SESSION['user_id'] = $fetch_users['userId'];
+            $_SESSION['user_surname'] = $fetch_users['userSurname'];
+            header('location:home.php');
+         }
+      }
+   } else {
+      $message[] = 'Invalid email or password';
+   }
 
 }
 
@@ -36,6 +37,7 @@ if(isset($_POST['submit'])){
 
 <!DOCTYPE html>
 <html lang="cs">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -45,32 +47,43 @@ if(isset($_POST['submit'])){
    <link rel="stylesheet" href="css/style.css">
 
 </head>
+
 <body>
 
-<?php
-if(isset($message)){
-   foreach($message as $message){
-      echo '
-      <div class="message">
-         <span>'.$message.'</span>
-         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
-      </div>
-      ';
+   <?php
+   if (isset($message)) {
+      foreach ($message as $message) {
+         echo '
+         <div class="message">
+            <span>' . $message . '</span>
+            <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+            <script>
+               setTimeout(function() {
+                  document.querySelector(".message").style.opacity = "0";
+                  document.querySelector(".message").style.transition = "all 0.5s";
+                  setTimeout(function() {
+                     document.querySelector(".message").remove();
+                  }, 500);
+               }, 3500);
+            </script>
+         </div>
+         ';
+      }
    }
-}
-?>
-   
-<div class="form-container">
+   ?>
 
-   <form action="" method="post">
-      <h3>Login</h3>
-      <input type="email" name="email" placeholder="Email"  class="box" required>
-      <input type="password" name="password" placeholder="Password" class="box" required>
-      <input type="submit" name="submit" value="Login" class="btn">
-      <p>Don't have an account? <a href="register.php">Request an account</a></p>
-   </form>
+   <div class="form-container">
 
-</div>
+      <form action="" method="post">
+         <h3>Login</h3>
+         <input type="email" name="email" placeholder="Email" class="box" required>
+         <input type="password" name="password" placeholder="Password" class="box" required>
+         <input type="submit" name="submit" value="Login" class="btn">
+         <p>Don't have an account? <a href="register.php">Request an account</a></p>
+      </form>
+
+   </div>
 
 </body>
+
 </html>
