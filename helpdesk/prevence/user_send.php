ced79ed35541b492038d0d7978dbe072827c90d5
yenda420 <161469951+yenda420@users.noreply.github.com>
2024-03-04 15:58:15 +0000

    titles fixed

--- helpdesk/prevence/user_send.php
+++ helpdesk/prevence/user_send.php
@@ -29,7 +29,7 @@ if(isset($_POST['send_btn'])) {
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Techbase</title>
+   <title>Send a ticket</title>
 
 
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
