a9b5a81f6ba9c9f0bec47780d688806408a85ee8
yenda420 <161469951+yenda420@users.noreply.github.com>
2024-03-12 13:25:45 +0000

    vendy me bije

--- helpdesk/prevence/searchbar.php
+++ helpdesk/prevence/searchbar.php
@@ -1,16 +1,15 @@
 <?php
 require('functions.php');
-//error_reporting(E_ALL ^ E_WARNING);
 
 session_start();
 if (isset($_SESSION['admin_id'])) {
-   $admin_id = $_SESSION['admin_id'];
+    $admin_id = $_SESSION['admin_id'];
 } else {
-   header('location:index.php');
+    header('location:index.php');
 }
 
-if($_SESSION['department'][0] != 'Super-admin') {
-    $fullQuery .=  "AND (tps.departmentId = {$_SESSION['departmentId'][0]}";
+if ($_SESSION['department'][0] != 'Super-admin') {
+    $fullQuery .= "AND (tps.departmentId = {$_SESSION['departmentId'][0]}";
     foreach ($_SESSION['departmentId'] as $departmentId) {
         if ($departmentId != $_SESSION['departmentId'][0]) {
             $fullQuery .= " OR tps.departmentId = $departmentId";
@@ -22,7 +21,6 @@ if($_SESSION['department'][0] != 'Super-admin') {
 if (!isset($_POST['search'])) {
     $_POST['search'] = null;
 }
-
 
 $table_associative_array = array(
     'admins' => array(
@@ -63,9 +61,6 @@ $table_associative_array = array(
         'userEmail'
     )
 );
-
-//var_dump($table_associative_array); 
-
 ?>
 
 <!DOCTYPE html>
@@ -104,24 +99,23 @@ $table_associative_array = array(
                     <div class="inputBox">
                         <button type="submit" name="search" class="btn">Search</button>
                     </div><br>
-
-                    
                     <div class="box-container">
-
-                    <?php
-                        
-                        if (isset($_POST['search'])) {
-                            if (isset($_POST['keyword'])) {
-                                $search_keyword = $_POST['keyword'];
-                                php_search_all_database($search_keyword, $table_associative_array);
+                        <div class='emptyWrap'>
+                            <div class='emptyDiv'>
+                                <p class='empty'><span style='color:black;'>Searched Keyword: </span><?= $_POST['keyword'] ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-container">
+                        <?php
+                            if (isset($_POST['search'])) {
+                                if (isset($_POST['keyword'])) {
+                                    $search_keyword = $_POST['keyword'];
+                                    php_search_all_database($search_keyword, $table_associative_array);
+                                }
                             }
-                        }
-
                         ?>
-
-                    </div>    
-
-                        
+                    </div>
                 </div>
             </form>
         </section>
@@ -129,97 +123,68 @@ $table_associative_array = array(
     <script src="js/admin_script.js"></script>
     <?php include 'footer.php'; ?>
 </body>
-
 </html>
 
 <?php
-
-//error_reporting(E_ALL ^ E_WARNING);
-
-//$search_keyword = "admin";						// Enter keyword to search
-
-
-
-//var_dump($table_associative_array); 
-
-
-
 function php_search_all_database($search_keyword, $table_associative_array)
 {
-
     global $conn;
     $conn = mysqli_connect("172.31.1.103", "helpdesk", "Helpdesk.123", "helpdesk");
-    $conn -> set_charset("utf8");
-
+    $conn->set_charset("utf8");
     $count = 0;
 
     if (mysqli_connect_errno()) {		// Check if database connection is ok
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
 
-    echo "<div class='emptyWrap'><div class='emptyDiv'><p class='empty'><span style='color:black;'>Searched Keyword : </span>" . $search_keyword . '</p></div></div>';
-    
     if (count($table_associative_array) > 0) {
-
         foreach ($table_associative_array as $table_name => $columnn_name) {
-
             foreach ($columnn_name as $column) {
-
                 $db_search_result_fields = $column . " LIKE ('%" . $search_keyword . "%')";		// We have used wildcards as an example, You can replace as per your need
                 $db_search_result = $conn->query("SELECT * FROM " . $table_name . " WHERE " . $db_search_result_fields);
-                
+
                 if ($db_search_result->num_rows > 0) {
                     while ($row = $db_search_result->fetch_array()) {
-                        if($table_name == 'admins'){
+                        if ($table_name == 'admins') {
                             $columnName = substr($column, 5);
-                        } else if($table_name == 'users'){
+                        } else if ($table_name == 'users') {
                             $columnName = substr($column, 4);
-                        } else if($table_name == 'requests'){
-                            $columnName = substr($column,3);
+                        } else if ($table_name == 'requests') {
+                            $columnName = substr($column, 3);
                         }
-                        
+
                         echo '<div class="box">';
+                            if (($table_name == 'admins') or ($table_name == 'users')) {
+                                echo '<div class="breaking"><p> Page : <span><a href="users.php"> All users
+                                </a></span></p></div>';
+                            } else if ($table_name == 'requests') {
+                                echo '<div class="breaking"><p> Page : <span><a href="admin_page.php"> Requests
+                                </a></span></p></div>';
+                            } else if ($table_name == 'departments') {
+                                echo '<div class="breaking"><p> Page : <span><a href="departments.php"> Departments
+                                </a></span></p></div>';
+                            } else if ($table_name == 'tickets') {
+                                echo '<div class="breaking"><p> Page : <span><a href="admin_tickets.php"> Tickets
+                                </a></span></p></div>';
+                            } else if ($table_name == 'ticket_types') {
+                                echo '<div class="breaking"><p> Page : <span><a href="tck_types.php"> Ticket types
+                                </a></span></p></div>';
+                            }
 
-                        if(($table_name == 'admins') or ($table_name == 'users')) {
-                            echo '<div class="breaking"><p> Page : <span><a href="users.php"> All users
-                            </a></span></p></div>';
-                        } else if($table_name == 'requests') {
-                            echo '<div class="breaking"><p> Page : <span><a href="admin_page.php"> Requests
-                            </a></span></p></div>';
-                        } else if($table_name == 'departments') {
-                            echo '<div class="breaking"><p> Page : <span><a href="departments.php"> Departments
-                            </a></span></p></div>';
-                        } else if($table_name == 'tickets') {
-                            echo '<div class="breaking"><p> Page : <span><a href="admin_tickets.php"> Tickets
-                            </a></span></p></div>';
-                        } else if($table_name == 'ticket_types') {
-                            echo '<div class="breaking"><p> Page : <span><a href="tck_types.php"> Ticket types
-                            </a></span></p></div>';
-                        }
-
-                        echo '<div class="breaking"><p>Column name : <span>' . $columnName . "</span></p></div>";
-                        echo '<div class="breaking"><p>Row : <span>' . $row[0] . "</span></p></div>";
-                        echo '<div class="breaking"><p>Value : <span>' . $row[$column] . "</span></p></div></div>";
-
+                            echo '<div class="breaking"><p>Column name : <span>' . $columnName . "</span></p></div>";
+                            echo '<div class="breaking"><p>Row : <span>' . $row[0] . "</span></p></div>";
+                            echo '<div class="breaking"><p>Value : <span>' . $row[$column] . "</span></p></div>";
+                        echo '</div>';
                     }
-
-
                 } else {
                     if ($db_search_result->num_rows == 0) {
-                     $count++;   
+                        $count++;
                     }
                 }
-
             }
-
-            //echo $table_name . " search End's Here<hr><br>";
-
         }
-
-    } 
-
-    if($count >= 24) {
-        echo '<div class="emptyWrap"><div class="emptyDiv"><p class="empty"><span style="color:black"> No results found for </span>'. $search_keyword .' </p></div></div>';
     }
-
+    if ($count >= 24) {
+        echo '<div class="emptyWrap"><div class="emptyDiv"><p class="empty"><span style="color:black"> No results found for </span>' . $search_keyword . ' </p></div></div>';
+    }
 }
