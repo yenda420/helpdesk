3ad5d0ab2f3bb01f73a221249cc6457407a32c21
yenda420 <161469951+yenda420@users.noreply.github.com>
2024-03-27 10:19:08 +0100

    config

--- helpdesk/prevence/config.php
+++ helpdesk/prevence/config.php
@@ -1,4 +1,4 @@
 <?php
 
-$conn = mysqli_connect("172.31.1.103", "helpdesk", "Helpdesk.123", "helpdesk") or die("Connection failed");
+$conn = new mysqli("localhost", "root", "", "helpdesk") or die("Connection failed");
 $conn -> set_charset("utf8");
