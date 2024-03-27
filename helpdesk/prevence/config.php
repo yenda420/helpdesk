63f0511cd42685ad0c26f4d8517175e06a4217aa
yenda420 <161469951+yenda420@users.noreply.github.com>
2024-03-27 10:19:56 +0100

    config

--- helpdesk/prevence/config.php
+++ helpdesk/prevence/config.php
@@ -2,3 +2,9 @@
 
 $conn = new mysqli("localhost", "root", "", "helpdesk") or die("Connection failed");
 $conn -> set_charset("utf8");
+
+// Check connection
+if ($mysqli -> conn) {
+    echo "Failed to connect to MySQL: " . $mysqli -> conn;
+    exit();
+  }
