605afd456d054fcc793b7a5ce256b93ddfe5c910
yenda420 <161469951+yenda420@users.noreply.github.com>
2024-05-27 08:08:47 +0200

    vkládání dat do messages po odpovědi uživatele

--- helpdesk/src/user_messages.php
+++ helpdesk/src/user_messages.php
@@ -16,13 +16,34 @@ if (isset($_POST['helped'])) {
     $stmt->bind_param("i", $_POST['ticket_id']);
     $stmt->execute();
 
-    if ($stmt->affected_rows <= 0) 
+    if ($stmt->affected_rows <= 0)
         $_SESSION['message'] = "Error updating database";
 
     $stmt->close();
 
     header("Location: " . $_SERVER['PHP_SELF']);
     exit;
+}
+
+if (isset($_POST['usr_msg_send'])) {
+    $query = '
+        INSERT INTO messages (conversationId, msgContent, senderUserId)
+        VALUES (?, ?, ?)
+    ';
+
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("isi", $_POST['convo_id'], $_POST['usr_msg'], $_POST['admin_id']);
+    $insert_msg_query = $stmt->execute();
+    $stmt->close();
+
+    if ($insert_msg_query) {
+        $_SESSION['message'] = "Message sent successfully";
+    } else {
+        $_SESSION['message'] = "Failed to send message";
+    }
+
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit; 
 }
 ?>
 
@@ -44,14 +65,16 @@ if (isset($_POST['helped'])) {
             <h1 class="title">Your Messages</h1>
             <div class="box-container">
                 <?php
-                    foreach (returnConvos($conn, 88) as $convo) {
-                        foreach(returnMessages($conn, $convo["convoId"]) as $message) {
-                            $senderName = returnAdmin($conn, $message["senderAdminId"])['adminName'] . " " . 
-                                            returnAdmin($conn, $message["senderAdminId"])['adminSurname'];
+                foreach (returnConvos($conn, 88) as $convo) {
+                    foreach (returnMessages($conn, $convo["convoId"]) as $message) {
+                        /* Zatím to tu hází error, opravím později */
+                        if ($message["senderAdminId"] !== "NULL") {
+                            $senderName = returnAdmin($conn, $message["senderAdminId"])['adminName'] . " " .
+                                returnAdmin($conn, $message["senderAdminId"])['adminSurname'];
                             $senderEmail = returnAdmin($conn, $message["senderAdminId"])['adminEmail'];
                             $ticketTitle = returnTicket($conn, $convo["ticketId"])['title'];
                             $msgContent = $message["msgContent"]; ?>
-
+    
                             <div class="box">
                                 <div class="breaking">
                                     <p> Message from:
@@ -68,41 +91,41 @@ if (isset($_POST['helped'])) {
                                     </p>
                                 </div>
                                 <div class="breaking">
-                                    <p> Response for: 
+                                    <p> Response for:
                                         <span>
                                             <?= $ticketTitle ?>
-                                        </span> 
+                                        </span>
                                     </p>
                                 </div>
                                 <div class="breaking">
-                                    <p> Message: 
+                                    <p> Message:
                                         <span>
                                             <?= $msgContent ?>
-                                        </span> 
+                                        </span>
                                     </p>
                                 </div>
                                 <form method="POST" class="help-form">
                                     <div class="help-text">Did this help you?</div>
                                     <div>
+                                        <input type="hidden" name="convo_id" value="<?php echo $convo["convoId"]; ?>">
                                         <input type="hidden" name="ticket_id" value="<?php echo $convo["ticketId"]; ?>">
                                         <button type="submit" name="helped" class="btn">Yes</button>
-                                        <button name="didnt_help" class="delete-btn no-btn">No</button>
+                                        <button class="delete-btn no-btn">No</button>
                                         <div class="user-reply notActive">
-                                            <textarea name="usr_msg" id="usr_msg" class="msg_send_user_form" 
-                                            placeholder="Please describe your problem in more detail. Did our reply change the state of your issue?"></textarea>
+                                            <textarea name="usr_msg" id="usr_msg" class="msg_send_user_form"
+                                                placeholder="Please describe your problem in more detail. Did our reply change the state of your issue?"></textarea>
                                             <button type="submit" name="usr_msg_send" class="btn reply-send-btn">Send</button>
                                         </div>
                                     </div>
                                 </form>
                             </div>
-                <?php   }
+                <?php }
                     }
-                ?>
+                } ?>
             </div>
         </section>
     </section>
     <script src="js/script.js"></script>
     <?php include 'footer.php'; ?>
 </body>
-
 </html>
